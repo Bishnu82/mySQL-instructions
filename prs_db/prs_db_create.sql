a81b21fda08cd86a4c4f525ae8cd4a2cbf64d560
John Cawley <jcawleyiii@gmail.com>
2019-10-05 19:25:09 -0400

    combined my create with my add stuff

--- prs_db/prs_db_create.sql
+++ prs_db/prs_db_create.sql
@@ -39,7 +39,8 @@ create table request (
 	total				decimal(10,2)	not null,
 	submittedDate		datetime		not null,
 	reasonForRejection	varchar(100),
-		foreign key (userID) references user(id)
+		
+        foreign key (userID) references user(id)
     );
     
 create table product (
@@ -50,7 +51,8 @@ create table product (
 	price		decimal(10,2)	not null,
 	unit		varchar(255),
 	photoPath	varchar(255),
-		foreign key (vendorID) references vendor(id),
+		
+        foreign key (vendorID) references vendor(id),
 			constraint vendor_part unique (vendorID, partNumber)
     );
     
@@ -59,7 +61,28 @@ create table lineItem (
 	requestID	int		not null,
 	productID	int		not null,
 	quantity	int		not null,
-		foreign key (productID) references product(id),
+		
+        foreign key (productID) references product(id),
         foreign key (requestID) references request(id),
 			constraint req_pdt unique (requestID, productID)
     );
+    
+-- insert user into db (ID, userName, password, firstName, lastName, phoneNumber, email, isReviewer, isAdmin)
+insert into user (userName, password, firstName, lastName, phoneNumber, email, isReviewer, isAdmin) value
+	('JSmith', 'easy1234', 'John', 'Smith', '555-555-5555', 'jsmith@aol.com', 0, 0),
+    ('ASmith', 'easy1234', 'Arron', 'Smith', '555-555-5555', 'asmith@aol.com', 0, 0);
+    
+    
+-- insert vendor into db (code, name, address, city, state, zip, phoneNumber, email)
+insert into vendor (code, name, address, city, state, zip, phoneNumber, email) value
+	('12rty7', 'Citi', '1234 Long Rd', 'Cincinnati', 'OH', '45321', '555-555-5555', 'Citi@aol.com');
+    
+    
+-- insert product into db (vendorID, partNumber, name, price, unit, photoPath)
+insert into product (vendorID, partNumber, name, price, unit, photoPath) value
+	(1, 'a192o', 'potato', 3.25, 'idaho', 'C:\repos\mySQL-instruction\prs_db\photo)'),
+    (1, 'a1958', 'tv', 250.00, 'LG', 'C:\repos\mySQL-instruction\prs_db\photo)');
+    
+-- create a user and grant privileges to that user
+create user prs_user@localhost identified by 'sesame';
+grant select, insert, update, delete on prs_db.* to prs_user@localhost;
